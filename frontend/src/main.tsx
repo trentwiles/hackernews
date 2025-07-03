a7f5cf6d0093dfb8d6bb68f5504caf77e74ff566
trentwiles <wilest7@gmail.com>
2025-07-03 02:25:12 -0400

    disable strict mode + comment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+
+  // STRICT MODE IS DISABLED TO STOP DOUBLE REQUESTS
+  // https://chatgpt.com/share/68662232-cbf4-800a-8a3e-92aca82124c7
+
+  // <StrictMode>
     <App />
-  </StrictMode>,
+  // </StrictMode>,
 )
