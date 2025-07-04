de2c63d53bd077b1e15e27af0dded13b3904babb
trentwiles <wilest7@gmail.com>
2025-07-04 13:12:53 -0400

    correct sidebar errors

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,8 +1,10 @@
+import WebSidebar from "./fragments/WebSidebar";
 import DataTable from "./fragments/DataTable";
 export default function Home() {
   return (
-    <>
+    <div className="flex min-h-screen">
+      <WebSidebar />
       <DataTable />
-    </>
+    </div>
   );
 }
